6bad58bef6eaf722f8b24affcbc65c9f7de505ba
adyelson <adyelson.loureiro@gmail.com>
2023-02-25 21:12:34 -0300

    funciona em mouse

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// document.querySelector('.neutralArea').addEventListener('click',()=>{
-//      e.target
-//      e.currentTarget
-//  })
-
 let areas = {
     a: null,
     b: null,
@@ -12,12 +7,17 @@ let areas = {
 document.querySelectorAll('.item').forEach(item=>{
     item.addEventListener('dragstart', dragStart);
     item.addEventListener('dragend', dragEnd);
+    item.addEventListener('touchstart', touchStart);
+    item.addEventListener('touchend', touchEnd);
 })
 
 document.querySelectorAll('.area').forEach(area=>{
     area.addEventListener('dragover', dragOver);
     area.addEventListener('dragleave', dragLeave);
     area.addEventListener('drop', drop);
+    area.addEventListener('touchenter', touchEnter);
+    area.addEventListener('touchleave', touchLeave);
+    area.addEventListener('touchmove', touchMove);
 })
 
 //function item
@@ -27,6 +27,16 @@ function dragStart(e){
 }
 
 function dragEnd(e){
+    e.currentTarget.classList.remove('dragging');
+}
+
+function touchStart(e){
+    e.preventDefault();
+    e.currentTarget.classList.add('dragging');
+}
+
+function touchEnd(e){
+    e.preventDefault();
     e.currentTarget.classList.remove('dragging');
 }
 
@@ -41,7 +51,9 @@ function dragLeave(e){
     e.currentTarget.classList.remove('hover');
 
 }
+
 function drop(e){
+    e.preventDefault();
     e.currentTarget.classList.remove('hover');
     let dragItem = document.querySelector('.item.dragging');
     if(e.currentTarget.querySelector('.item') === null){
@@ -50,11 +62,28 @@ function drop(e){
     }
 }
 
+function touchEnter(e){
+    e.preventDefault();
+    e.currentTarget.classList.add('hover');
+}
+
+function touchLeave(e){
+    e.preventDefault();
+    e.currentTarget.classList.remove('hover');
+}
+
+function touchMove(e){
+    e.preventDefault();
+}
+
 //neutralarea
 
 document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral);
 document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral);
 document.querySelector('.neutralArea').addEventListener('drop', dropNeutral);
+document.querySelector('.neutralArea').addEventListener('touchenter', touchEnterNeutral);
+document.querySelector('.neutralArea').addEventListener('touchleave', touchLeaveNeutral);
+document.querySelector('.neutralArea').addEventListener('touchmove', touchMoveNeutral);
 
 //function neutral
 
@@ -65,13 +94,28 @@ function dragOverNeutral(e){
 function dragLeaveNeutral(e){
     e.currentTarget.classList.remove('hover');
 }
+
 function dropNeutral(e){
+    e.preventDefault();
     e.currentTarget.classList.remove('hover');
     let dragItem = document.querySelector('.item.dragging');
     e.currentTarget.appendChild(dragItem);
     updateAreas();
 }
 
+function touchEnterNeutral(e){
+    e.preventDefault();
+    e.currentTarget.classList.add('hover');
+}
+
+function touchLeaveNeutral(e){
+    e.preventDefault();
+    e.currentTarget.classList.remove('hover');
+}
+
+function touchMoveNeutral(e){
+    e.preventDefault();
+}
 
 //logic functions
 
